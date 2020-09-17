cc3d7734667ffb173820510d25d1b65a596aa17f
kvzivn <hello@kevinivan.com>
2020-09-17 15:44:45 +0200

    add back page generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-// const createPages = require("./create/createPages")
-// const createPosts = require("./create/createPosts")
+const createPages = require("./create/createPages")
+const createPosts = require("./create/createPosts")
 
-// exports.createPagesStatefully = async ({ graphql, actions, reporter }, options) => {
-//   await createPages({ actions, graphql, reporter }, options)
-//   await createPosts({ actions, graphql, reporter }, options)
-// }
+exports.createPagesStatefully = async ({ graphql, actions, reporter }, options) => {
+  await createPages({ actions, graphql, reporter }, options)
+  await createPosts({ actions, graphql, reporter }, options)
+}
