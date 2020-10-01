d7af33eda8e7f5d0bf7725d0f9218e4a094ed7ea
kvzivn <hello@kevinivan.com>
2020-10-02 00:48:08 +0200

    lets go

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,7 @@ import React from 'react'
 import MobileHomepage from '../components/MobileHomepage'
 import DesktopHomepage from '../components/DesktopHomepage'
 
-export default () => window.innerWidth > 900 ? <DesktopHomepage /> : <MobileHomepage />
+export default () => {
+  if (typeof window === 'undefined') return
+  window.innerWidth > 900 ? <DesktopHomepage /> : <MobileHomepage />
+}
