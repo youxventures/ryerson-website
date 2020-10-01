84fc713e0edc899bf5622fddbb52b403198348a8
kvzivn <hello@kevinivan.com>
2020-10-02 00:41:42 +0200

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import { isMobile } from 'react-device-detect'
 import MobileHomepage from '../components/MobileHomepage'
 import DesktopHomepage from '../components/DesktopHomepage'
 
-export default () => {
-  if (isMobile) {
-    return <MobileHomepage />
-  } else {
-    return <DesktopHomepage />
-  }
-}
+export default () => window.innerWidth > 900 ? <DesktopHomepage /> : <MobileHomepage />
