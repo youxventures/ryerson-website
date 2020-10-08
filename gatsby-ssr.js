6393802a7bcd1a06600f139e23167310f22ab6dd
kvzivn <hello@kevinivan.com>
2020-10-08 20:21:40 +0200

    comon

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,16 @@
-var React = require("react")
+import React from 'react'
 
-// export const onRenderBody = ({ setPostBodyComponents }) => {
-//   setPostBodyComponents([
-//     <script
-//       key="https://www.youtube.com/iframe_api"
-//       src="https://www.youtube.com/iframe_api"
-//     />
-//   ])
-// }
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="https://www.youtube.com/iframe_api"
+      src="https://www.youtube.com/iframe_api"
+    />
+  ])
+}
 
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  const headComponents = getHeadComponents();
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
 
   headComponents.sort((a, b) => {
     if (a.props && a.props["data-react-helmet"]) {
