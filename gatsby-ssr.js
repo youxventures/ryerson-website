9634c765f66f276128931e21559fcb828c6c2254
kvzivn <hello@kevinivan.com>
2020-10-08 20:05:09 +0200

    try again

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,29 +1,22 @@
-import React from 'react'
+var React = require("react")
 
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      key="https://www.youtube.com/iframe_api"
-      src="https://www.youtube.com/iframe_api"
-    />
-  ])
-}
+// export const onRenderBody = ({ setPostBodyComponents }) => {
+//   setPostBodyComponents([
+//     <script
+//       key="https://www.youtube.com/iframe_api"
+//       src="https://www.youtube.com/iframe_api"
+//     />
+//   ])
+// }
 
-export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  const headComponents = getHeadComponents()
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
 
   headComponents.sort((a, b) => {
-    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+    if (a.props && a.props["data-react-helmet"]) {
       return 0
     }
-
-    if (a.type === 'style') {
       return 1
-    } else if (b.type === 'style') {
-      return -1
-    }
-
-    return 0
   })
 
   replaceHeadComponents(headComponents)
