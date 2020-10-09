9e4a15f6e0259531f35d76d581d356163dda2a56
kvzivn <hello@kevinivan.com>
2020-10-09 15:44:40 +0200

    remove trailing slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-remove-trailing-slashes`,
     {
       resolve: 'gatsby-plugin-html-attributes',
       options: {
